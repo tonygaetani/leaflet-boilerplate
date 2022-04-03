42fc05afb60334f0aaa1061cd67eb1864f8d2812
Tony Gaetani <tony.gaetani@gmail.com>
2022-04-03 11:55:50 +0300

    change let to const

--- js/app.js
+++ js/app.js
@@ -226,7 +226,7 @@ function updateGameState(event) {
 ////////////////////
 function startGame() {
   // start the timer
-  let secondsLeft = 2 * 60; // 2 minutes
+  const secondsLeft = 2 * 60; // 2 minutes
   const gameTimerInterval = setInterval(function () {
     if (!game.pause) {
       secondsLeft -= 1;
